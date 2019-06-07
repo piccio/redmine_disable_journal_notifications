dd489ad2aa72c9f0df30d79b64da4f778d648684
Roberto Piccini <roberto.piccini@gmail.com>
2019-06-07 12:39:57 +0200

    bugfix: don't render on the 'new' action page

--- init.rb
+++ init.rb
@@ -11,7 +11,7 @@ Redmine::Plugin.register :redmine_disable_journal_notifications do
   name 'Redmine Disable Journal Notifications'
   author 'Roberto Piccini'
   description 'a user who updates a ticket can disable email notifications'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/piccio/redmine_disable_journal_notifications'
   author_url 'https://github.com/piccio'
 end
--- lib/redmine_disable_journal_notifications/hooks/view_issues_form_details_bottom_hook.rb
+++ lib/redmine_disable_journal_notifications/hooks/view_issues_form_details_bottom_hook.rb
@@ -1,7 +1,8 @@
 module RedmineDisableJournalNotifications
   module Hooks
     class ViewIssuesFormDetailsBottomHook < Redmine::Hook::ViewListener
-      render_on(:view_issues_form_details_bottom, partial: 'issues/disable_journal_notifications', layout: false)
+      render_on(:view_issues_form_details_bottom, partial: 'issues/disable_journal_notifications', layout: false,
+                locals: { render_condition: Proc.new { |controller| controller.action_name != 'new' } })
     end
   end
 end
